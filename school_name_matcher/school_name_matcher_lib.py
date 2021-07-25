7cdf841285617bad22a978bb997c92572c35b58b
Shashir Reddy <shashir@umich.edu>
2021-07-25 11:09:26 -0700

    Allow district name matching.

--- school_name_matcher/school_name_matcher_lib.py
+++ school_name_matcher/school_name_matcher_lib.py
@@ -30,29 +30,41 @@ STATE_ABBREV_MAPPING = {
     "NY": "New York",
     "TX": "Texas",
     "FL": "Florida",
+    "LA": "Louisiana",
+    "AR": "Arkansas",
+    "DE": "Delaware",
+    "IA": "Iowa",
+    "KY": "Kentucky",
+    "MA": "Massachusetts",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "UT": "Utah",
+    "WI": "Wisconsin",
 }
 
 SEARCH_STOP_WORDS = {"school", "district", "high", "middle", "elementary",
                      "academy", "charter"}
 
 
-def trigger_school_name_results(
-    query, schools_df, schools_search_index, threshold=0.3, num_results=1
-):
-  results = schools_search_index.get(query)
+def trigger_name_results(
+    query, filtered_nces_df, search_index, process_districts,
+    threshold=0.3, num_results=1):
+  results = search_index.get(query)
+  nces_column_to_match = "district_name" if process_districts else "sch_name"
   weighted_jaccard_scorer = \
     data_frame_text_search_lib.WeightedTokenJaccardScorer(
-        schools_df["sch_name"])
+        filtered_nces_df[nces_column_to_match])
   jaccard_scorer = data_frame_text_search_lib.TokenJaccardScorer()
   levenshtein_scorer = data_frame_text_search_lib.LevenshteinRatioScorer()
   scored_results = list()
   for result in results:
     hmean_scores = statistics.harmonic_mean([
         weighted_jaccard_scorer.score_df_result(
-            query, schools_df, "sch_name", result),
-        # jaccard_scorer.score_df_result(query, schools_df, "sch_name", result),
+            query, filtered_nces_df, nces_column_to_match, result),
+        # jaccard_scorer.score_df_result(query, filtered_nces_df,
+      # nces_column_to_match, result),
         levenshtein_scorer.score_df_result(
-            query, schools_df, "sch_name", result)
+            query, filtered_nces_df, nces_column_to_match, result)
     ])
     if hmean_scores == 1.0:
       return [(result, hmean_scores)]
@@ -62,55 +74,64 @@ def trigger_school_name_results(
   return scored_results[:num_results]
 
 
-def read_state_schools_csv(path: Text) -> pd.DataFrame:
-  """Reads school names for a given state from CSV."""
+def read_state_level_csv(path: Text) -> pd.DataFrame:
+  """Reads state level file containing either SchoolName or DistrictName for a
+  given state from CSV.
+  """
   state_abbrev_from_file_name = path.split("/")[-1][:2]
   df = pd.read_csv(path)
   # Validate
-  assert (list(set(df["StateAbbrev"]))[0] == state_abbrev_from_file_name)
+  assert (list(set(df["StateAbbrev"]))[0] == state_abbrev_from_file_name), (
+    "State abbreviation '%s' doesn't match state name '%s'" % (
+    list(set(df["StateAbbrev"]))[0], state_abbrev_from_file_name))
   assert (list(set(df["State"]))[0] == STATE_ABBREV_MAPPING[
     state_abbrev_from_file_name])
+  # Must contain SchoolName or DistrictName column.
+  assert "SchoolName" in df.columns or "DistrictName" in df.columns
   # Unique
   df.drop_duplicates(inplace=True, ignore_index=True)
   return df
 
 
 def best_effort_merge_nces_data(
-    state_df: pd.DataFrame, nces_schools_df: pd.DataFrame
+    state_df: pd.DataFrame, nces_df: pd.DataFrame,
+    process_districts: bool = False
 ) -> pd.DataFrame:
   state_abbrev = list(set(state_df["StateAbbrev"]))[0]
   # Get NCES data for state.
-  state_schools_df = nces_schools_df[
-    nces_schools_df["state"] == state_abbrev]
-  state_schools_df.reset_index(drop=True, inplace=True)
-  schools_search_index =\
-      data_frame_text_search_lib.DataFrameTextSearchInvertedIndex(
-          state_schools_df, "sch_name")
+  filtered_nces_df = nces_df[
+    nces_df["state"] == state_abbrev]
+  filtered_nces_df.reset_index(drop=True, inplace=True)
+  search_index = data_frame_text_search_lib.DataFrameTextSearchInvertedIndex(
+    filtered_nces_df,
+    "district_name" if process_districts else "sch_name")
   output = list()
   progress = ProgressBar(len(state_df.index), note=state_abbrev)
   for index, row in state_df.iterrows():
-    query = row["SchoolName"]
+    query = row["DistrictName" if process_districts else "SchoolName"]
     if pd.isna(query):
       continue
-    scored_results = trigger_school_name_results(
-        query, state_schools_df, schools_search_index)
+    scored_results = trigger_name_results(
+        query, filtered_nces_df, search_index, process_districts)
     has_results = False
     for scored_result in scored_results:
       output_row = [row[c] for c in state_df.columns] +\
-                   [state_schools_df.iloc[scored_result[0]][c]
-                    for c in state_schools_df.columns] +\
+                   [filtered_nces_df.iloc[scored_result[0]][c]
+                    for c in filtered_nces_df.columns] +\
                    [scored_result[1]]
       output.append(output_row)
       has_results = True
     if not has_results:
       output_row = [row[c] for c in state_df.columns] + \
-                   [None for _ in state_schools_df.columns] + \
+                   [None for _ in filtered_nces_df.columns] + \
                    [None]
       output.append(output_row)
     progress.increment()
   progress.complete()
-  return pd.DataFrame(
+  output_df = pd.DataFrame(
       output,
       columns=(
-          list(state_df.columns) + list(state_schools_df.columns) + [
+          list(state_df.columns) + list(filtered_nces_df.columns) + [
           "match_score"]))
+  output_df.sort_values(by=["match_score"], inplace=True)
+  return output_df
--- school_name_matcher/school_name_matcher_main.py
+++ school_name_matcher/school_name_matcher_main.py
@@ -7,25 +7,45 @@ from school_name_matcher import school_name_matcher_lib
 FLAGS = flags.FLAGS
 flags.DEFINE_list("input_files", [], "Files containing SchoolName column.")
 flags.DEFINE_string("nces_schools", None, "NCES school data.")
+flags.DEFINE_string("nces_districts", None, "NCES district data.")
 
 
 def main(argv):
   assert len(argv) == 1, "Unexpected arguments provided: " + " ".join(argv[1:])
-  nces_schools_df = pd.read_csv(
-      FLAGS.nces_schools,
+  process_districts = False
+  nces_df = None
+  if FLAGS.nces_districts:
+    process_districts = True
+    assert not FLAGS.nces_schools, (
+        "Provide either NCES School data or NCES District data, but not both.")
+    nces_df = pd.read_csv(
+      FLAGS.nces_districts,
       dtype={
           "state": pd.StringDtype(),
           "district_name": pd.StringDtype(),
           "state_leaid": pd.StringDtype(),
-          "leaid": pd.StringDtype(),
-          "sch_name": pd.StringDtype(),
-          "ncessch": pd.StringDtype(),
-          "state_schid": pd.StringDtype(),
+          "leaid": pd.StringDtype()
       })
+  else:
+    assert not FLAGS.nces_districts, (
+        "Provide either NCES School data or NCES District data, but not both.")
+    nces_df = pd.read_csv(
+        FLAGS.nces_schools,
+        dtype={
+            "state": pd.StringDtype(),
+            "district_name": pd.StringDtype(),
+            "state_leaid": pd.StringDtype(),
+            "leaid": pd.StringDtype(),
+            "sch_name": pd.StringDtype(),
+            "ncessch": pd.StringDtype(),
+            "state_schid": pd.StringDtype(),
+        })
+
+
   for input_file in FLAGS.input_files:
-    state_df = school_name_matcher_lib.read_state_schools_csv(input_file)
+    state_df = school_name_matcher_lib.read_state_level_csv(input_file)
     joined_df = school_name_matcher_lib.best_effort_merge_nces_data(
-        state_df, nces_schools_df)
+        state_df, nces_df, process_districts)
     joined_df.to_csv(input_file.replace(".csv", "_with_NCES_matches.csv"))
 
 
