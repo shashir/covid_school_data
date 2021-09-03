9d54ee85a4ee8c5e375476e9f6540ff0a28c4434
Shashir Reddy <shashir@umich.edu>
2021-09-02 20:35:26 -0700

    Join additional NCES metadata.

---
+++ additional_nces_data_joiner/additional_nces_data_joiner_lib.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+from typing import Dict, List, Text
+
+def format_id(id: Text, length: int) -> Text:
+  return str(int(float(id))).strip().zfill(length)
+
+def comma_separated_multilookup(map: Dict[Text, Text], key: Text):
+  if str(key) == "<NA>":
+    return None
+  keys: List[Text] = key.split(",")
+  values: List[Text] = []
+  for k in keys:
+    if k in map:
+      values.append(map[k])
+  return ",".join(values)
+
+def join_nces_school_data(
+    state_case_df,
+    nces_school_demographics_df,
+    nces_district_demographics_df):
+  state_leaid_lookup = {
+      format_id(k, 12): v
+      for k, v in nces_school_demographics_df[
+        ["ncessch_num", "state_leaid"]].dropna().values
+  }
+  state_case_df["StateAssignedDistrictID"] = state_case_df["NCESSchoolID"].map(
+      lambda key: comma_separated_multilookup(state_leaid_lookup, key))
+
+  # Unfortunately, we can only look up district type using
+  # nces_district_demographics_df
+  agency_type_lookup = {
+      format_id(k, 7): v
+      for k, v in nces_district_demographics_df[
+        ["leaid", "agency_type"]].dropna().values
+  }
+  state_case_df["DistrictType"] = state_case_df["NCESDistrictID"].map(
+      lambda key: comma_separated_multilookup(agency_type_lookup, key))
+
+  # We can only look up DistrictName using nces_district_demographics_df
+  lea_name_lookup = {
+      format_id(k, 7): v
+      for k, v in nces_district_demographics_df[
+        ["leaid", "lea_name"]].dropna().values
+  }
+  state_case_df["DistrictName"] = state_case_df["NCESDistrictID"].map(
+      lambda key: comma_separated_multilookup(lea_name_lookup, key))
+
+  charter_lookup = {
+      format_id(k, 12): v
+      for k, v in nces_school_demographics_df[
+        ["ncessch_num", "charter"]].dropna().values
+  }
+  state_case_df["Charter"] = state_case_df["NCESSchoolID"].map(
+      lambda key: comma_separated_multilookup(charter_lookup, key))
+
+  school_type_lookup = {
+      format_id(k, 12): v
+      for k, v in nces_school_demographics_df[
+        ["ncessch_num", "school_type"]].dropna().values
+  }
+  state_case_df["SchoolType"] = state_case_df["NCESSchoolID"].map(
+      lambda key: comma_separated_multilookup(school_type_lookup, key))
+
+  seasch_type_lookup = {
+      format_id(k, 12): v
+      for k, v in nces_school_demographics_df[
+        ["ncessch_num", "seasch"]].dropna().values
+  }
+  state_case_df["StateAssignedSchoolID"] = state_case_df["NCESSchoolID"].map(
+      lambda key: comma_separated_multilookup(seasch_type_lookup, key))
+  return state_case_df
+
+def join_nces_district_data(state_case_df, nces_df):
+  state_leaid_lookup = {
+      format_id(k, 7): v
+      for k, v in nces_df[["leaid", "state_leaid"]].dropna().values
+  }
+  state_case_df["StateAssignedDistrictID"] = state_case_df[
+    "NCESDistrictID"
+  ].map(lambda key: comma_separated_multilookup(state_leaid_lookup, key))
+
+  agency_type_lookup = {
+      format_id(k, 7): v
+      for k, v in nces_df[["leaid", "agency_type"]].dropna().values
+  }
+  state_case_df["DistrictType"] = state_case_df["NCESDistrictID"].map(
+      lambda key: comma_separated_multilookup(agency_type_lookup, key))
+
+  charter_lookup = {
+      format_id(k, 7): v
+      for k, v in nces_df[["leaid", "charter"]].dropna().values
+  }
+  state_case_df["Charter"] = state_case_df["NCESDistrictID"].map(
+      lambda key: comma_separated_multilookup(charter_lookup, key))
+
+  return state_case_df
---
+++ additional_nces_data_joiner/additional_nces_data_joiner_main.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+from absl import app
+from absl import flags
+from additional_nces_data_joiner import additional_nces_data_joiner_lib
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("state_case_data_csv", None, "Case data for a state.")
+flags.DEFINE_string("nces_school_demographics_csv", None,
+                    "NCES school demographics data.")
+flags.DEFINE_string("nces_district_demographics_csv", None,
+                    "NCES district demographics data.")
+flags.DEFINE_bool("process_districts", False,
+                  "Whether to only process districts")
+flags.DEFINE_string("output_csv", None, "Output CSV.")
+
+def main(argv):
+  assert len(argv) == 1, "Unexpected arguments provided: " + " ".join(argv[1:])
+  state_case_df = pd.read_csv(
+      FLAGS.state_case_data_csv,
+      dtype={
+          "NCESDistrictID": pd.StringDtype(),
+          "NCESSchoolID": pd.StringDtype(),
+      }
+  )
+  nces_school_demographics_df = pd.read_csv(
+      FLAGS.nces_school_demographics_csv,
+      usecols=["state_leaid", "leaid", "charter", "school_type",
+               "ncessch_num", "seasch"],
+      dtype={
+          "state_leaid": pd.StringDtype(),  # StateAssignedDistrictID
+          "leaid": pd.StringDtype(),  # NCESDistrictID
+          "charter": pd.StringDtype(),  # Charter
+          "school_type": pd.StringDtype(),  # SchoolType
+          "ncessch_num": pd.StringDtype(),  # NCESSchoolID
+          "seasch": pd.StringDtype(),  # StateAssignedDistrictID
+      })
+  nces_district_demographics_df = pd.read_csv(
+      FLAGS.nces_district_demographics_csv,
+      usecols=["lea_name", "state_leaid", "leaid", "agency_type", "charter"],
+      dtype={
+          "lea_name": pd.StringDtype(),  # DistrictName
+          "state_leaid": pd.StringDtype(),  # StateAssignedDistrictID
+          "leaid": pd.StringDtype(),  # NCESDistrictID
+          "agency_type": pd.StringDtype(),  # DistrictType
+          "charter": pd.StringDtype(),  # Charter
+      })
+
+  if FLAGS.process_districts:
+    output_df = additional_nces_data_joiner_lib.join_nces_district_data(
+        state_case_df, nces_district_demographics_df)
+  else:
+    output_df = additional_nces_data_joiner_lib.join_nces_school_data(
+        state_case_df, nces_school_demographics_df,
+        nces_district_demographics_df)
+  output_df.to_csv(FLAGS.output_csv, index=False)
+
+
+if __name__ == '__main__':
+  app.run(main)
