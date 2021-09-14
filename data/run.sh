eae18ffa93f08b95f52d1b4c37fba328ee6023d0
Shashir Reddy <shashir@umich.edu>
2021-09-13 20:25:41 -0700

    Upload all data files.

---
+++ data/run.sh
@@ -0,0 +1,8 @@
+python3 ../state_data_mapper/state_data_mapper_main.py \
+	--config "../state_data_mapper/state_data_mapper.conf" \
+	--report="./report.csv" \
+	--state_data_dir="./" \
+	--required_columns=StateName,StateAbbrev,DataLevel,Charter,SchoolName,SchoolType,NCESSchoolID,DistrictName,DistrictType,NCESDistrictID,County,TimePeriodInterval,TimePeriodStart,TimePeriodEnd,NewCasesCombined,NewCasesStudents,NewCasesStaff,NewCasesUnknown,TwoWeekPeriodStaffStudentCasesCombined,TwoWeekPeriodCasesStudents,TwoWeekPeriodCasesStaff,CumulativeCasesCombined,CumulativeCasesStudents,CumulativeCasesStaff,CumulativeCasesUnknown,ActiveCasesStaff,ActiveCasesStudents,ActiveCasesUnknown,ActiveCasesCombined \
+	--states_to_process="" \
+	--nces_school_metadata="./nces_metadata/NCES_School_Metadata.xlsx" \
+	--nces_district_metadata="./nces_metadata/NCES_District_Metadata.xlsx"
