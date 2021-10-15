2ad9cd232dd0d698c27a14388e9302d73949d6d2
Shashir Reddy <shashir@umich.edu>
2021-10-15 15:54:57 -0700

    Renamed output files.

--- data/run.sh
+++ data/run.sh
@@ -3,6 +3,6 @@ python3 ../state_data_mapper/state_data_mapper_main.py \
 	--report="./report.csv" \
 	--state_data_dir="./" \
 	--required_columns=StateName,StateAbbrev,DataLevel,Charter,SchoolName,SchoolType,NCESSchoolID,DistrictName,DistrictType,NCESDistrictID,County,TimePeriodInterval,TimePeriodStart,TimePeriodEnd,NewCasesCombined,NewCasesStudents,NewCasesStaff,NewCasesUnknown,TwoWeekPeriodStaffStudentCasesCombined,TwoWeekPeriodCasesStudents,TwoWeekPeriodCasesStaff,CumulativeCasesCombined,CumulativeCasesStudents,CumulativeCasesStaff,CumulativeCasesUnknown,ActiveCasesStaff,ActiveCasesStudents,ActiveCasesUnknown,ActiveCasesCombined \
-	--states_to_process="" \
+	--states_to_process="$1" \
 	--nces_school_metadata="./nces_metadata/NCES_School_Metadata.xlsx" \
 	--nces_district_metadata="./nces_metadata/NCES_District_Metadata.xlsx"
